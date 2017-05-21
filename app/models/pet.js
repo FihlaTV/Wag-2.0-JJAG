f9c147c2b9ee2882f4cfb3cdffa9905926c8911e
Ariel <ariel.m.little@gmail.com>
2017-05-20 21:52:18 -0400

    pet model

---
+++ app/models/pet.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Ariel on 5/20/2017.
+ */
+module.exports = function(sequelize, Datatypes) {
+
+    var Pet = sequelize.define('pet', {
+
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Datatypes.INTEGER
+        },
+
+        pet_name: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+
+        pet_type: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+
+        notes: {
+            type: Datatypes.STRING
+        },
+
+        img_link: {
+            type: Datatypes.STRING
+        }
+
+
+    });
+
+    return Pet;
+
+};
