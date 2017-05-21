cb8a1bfe1bea1d0c9633d1fe9214e1b73643b46e
Ariel <ariel.m.little@gmail.com>
2017-05-20 21:49:14 -0400

    owner model

--- app/models/owner.js
+++ app/models/owner.js
@@ -1,3 +1,57 @@
 /**
  * Created by Ariel on 5/20/2017.
  */
+module.exports = function(sequelize, Datatypes) {
+
+    var Owner = sequelize.define('owner', {
+
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Datatypes.INTEGER
+        },
+
+        first_name: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+
+        last_name: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+
+        address: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+
+        email: {
+            type: Datatypes.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
+
+        phone: {
+            type: Datatypes.INT,
+            validate: {
+                isNumeric: true
+            }
+        }
+
+
+    });
+
+    return Owner;
+
+};
